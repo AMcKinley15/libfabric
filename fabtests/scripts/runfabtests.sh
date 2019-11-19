46aee4e1ddf6cd1ad41e03e7f75a9d64a82b7cf2
Alex McKinley <alex@mckpals.com>
2019-11-19 15:15:39 -0800

    temporarily remove other tests to increase testing speed

--- fabtests/scripts/runfabtests.sh
+++ fabtests/scripts/runfabtests.sh
@@ -627,7 +627,7 @@ function main {
 	set_excludes
 
 	if [[ $1 == "quick" ]]; then
-		local -r tests="unit functional short multinode"
+		local -r tests="multinode"
 	elif [[ $1 == "verify" ]]; then
 		local -r tests="complex"
 		complex_type=$1
